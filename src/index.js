7951d7c399b88afa467d5a2142a5c09867ad9822
lzhalloran <lzhalloran@gmail.com>
2024-02-20 02:02:32 +1000

    removed HOST from server running code - Heroku Deploy

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // Import the configured items from the server file:
-var { app, PORT, HOST } = require("./server");
+var { app, PORT } = require("./server");
 
 // Run the server
-app.listen(PORT, HOST, () => {
+app.listen(PORT, () => {
   console.log(`
   ExpressJS Social Gaming API is now running!
   `);
