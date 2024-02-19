a2885cd850ae47b1d4fe0bb9d2bfa83e59126bd6
lzhalloran <lzhalloran@gmail.com>
2024-02-20 00:17:32 +1000

    connect server to database, databaseHealth route

--- src/server.js
+++ src/server.js
@@ -37,11 +37,57 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Configure Mongoose Database, depending on environment
+const mongoose = require('mongoose');
+var databaseURL = "";
+switch (process.env.NODE_ENV.toLowerCase()){
+    case "test":
+        databaseURL = "mongodb://localhost:27017/T3A2API-test";
+        break;
+    case "development":
+        databaseURL = "mongodb://localhost:27017/T3A2API-dev";
+        break;
+    case "production":
+        databaseURL = process.env.DATABASE_URL;
+        break;
+    default:
+        console.error("Incorrect JS environment specified, database not connected");
+        break;
+}
+// Import database connection functionality
+const {databaseConnector} = require('./database');
+// Attempt connection to the database
+databaseConnector(databaseURL).then(() => {
+    console.log(`  Database connected successfully!
+    `);
+}).catch(error => {
+    console.log(`
+    Some error occurred while connecting to the database! Error Details:
+    ${error}
+    `)
+});
+// A route that returns useful database connection details.
+// More details here: 
+// https://mongoosejs.com/docs/api/connection.html
+app.get("/databaseHealth", (request, response) => {
+    let databaseState = mongoose.connection.readyState;
+    let databaseName = mongoose.connection.name;
+    let databaseModels = mongoose.connection.modelNames();
+    let databaseHost = mongoose.connection.host;
+
+    response.json({
+        readyState: databaseState,
+        dbName: databaseName,
+        dbModels: databaseModels,
+        dbHost: databaseHost
+    });
+});
+
 // A temporary route to test initial server is functioning.
 // This path is the server API's "homepage".
 app.get("/", (request, response) => {
   response.json({
-    message: "Hello world!",
+    message: "Welcome to T3A2 API!",
   });
 });
 
