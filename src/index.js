298cd3ec0b9d89634ad56b12196edab5478ba85c
lzhalloran <lzhalloran@gmail.com>
2024-02-19 15:04:15 +1000

    initial code to run the server (index.js)

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Import the configured items from the server file:
+var { app, PORT, HOST } = require("./server");
+
+// Run the server
+app.listen(PORT, HOST, () => {
+  console.log(`ExpressJS Social Gaming API is now running!`);
+});
