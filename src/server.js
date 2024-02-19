3307c96c7ee1ef3b298dbcefb18f57fc4c445f2b
lzhalloran <lzhalloran@gmail.com>
2024-02-19 14:57:00 +1000

    initial server configuration

---
+++ src/server.js
@@ -0,0 +1,62 @@
+// Import .env data for server configuration
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Import Express and create an express app instance
+const express = require("express");
+const app = express();
+// Use .env data or assign default values
+const HOST = process.env.HOST || "localhost";
+const PORT = process.env.PORT || 3000;
+
+// Configure some basic Helmet setings on the server instance
+const helmet = require("helmet");
+app.use(helmet());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+    },
+  })
+);
+
+// Configure some basic CORS settings on the server instance.
+// Any front-end that should be allowed to interact with this API
+// Should be listed in the origins array for CORS configuration.
+// "https://deployedApp.com" is a placeholder
+const cors = require("cors");
+var corsOptions = {
+  origin: ["http://localhost:5000", "https://deployedApp.com"],
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+// Configure API-friendly request data formatting
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// A temporary route to test initial server is functioning.
+// This path is the server API's "homepage".
+app.get("/", (request, response) => {
+  response.json({
+    message: "Hello world!",
+  });
+});
+
+// Keep this route at the end, before exports.
+// A 404 route should only trigger if no preceding routes or middleware was run.
+app.get("*", (request, response) => {
+  response.status(404).json({
+    message: "No route with that path found!",
+    attemptedPath: request.path,
+  });
+});
+
+// Exports needed to run the server.
+module.exports = {
+  HOST,
+  PORT,
+  app,
+};
