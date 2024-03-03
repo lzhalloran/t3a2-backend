05461b68d8c14489b5937148f77dd88bc4f2ac96
lzhalloran <lzhalloran@gmail.com>
2024-03-03 23:05:09 +1000

    reject friend request

--- tests/controllers/FriendRoutes.test.js
+++ tests/controllers/FriendRoutes.test.js
@@ -230,3 +230,110 @@ describe("Accept friend request route...", () => {
     expect(response.body).toHaveProperty("error");
   });
 });
+
+// Reject Friend request route
+describe("Reject friend request route...", () => {
+  // Import mongoose for database functionality
+  const mongoose = require("mongoose");
+  // Import connector and disconnector from database to test
+  const {
+    databaseConnector,
+    databaseDisconnector,
+  } = require("../../src/database");
+
+  let encryptedUser1JWT = "";
+  let encryptedUser2JWT = "";
+
+  // connect to the database, seed data
+  beforeAll(async () => {
+    await databaseConnector();
+    await User.deleteMany({});
+
+    // user1
+    let response = await request(app).post("/users/register").send({
+      email: "user1@email.com",
+      password: "password1",
+      username: "user1",
+      name: "User One",
+    });
+    let loginResponse = await request(app).post("/users/login").send({
+      username: "user1",
+      password: "password1",
+    });
+    encryptedUser1JWT = loginResponse._body;
+
+    // user2
+    response = await request(app).post("/users/register").send({
+      email: "user2@email.com",
+      password: "password2",
+      username: "user2",
+      name: "User Two",
+    });
+    loginResponse = await request(app).post("/users/login").send({
+      username: "user2",
+      password: "password2",
+    });
+    encryptedUser2JWT = loginResponse._body;
+
+    // friend request from user 1 to user 2
+    response = await request(app)
+      .post("/friends/add/user2")
+      .set("jwt", encryptedUser1JWT)
+      .send({});
+    encryptedUser1JWT = response.body.jwt;
+  });
+
+  // disconnect and clean up after using the database
+  afterAll(async () => {
+    await User.deleteMany({});
+    await databaseDisconnector();
+  });
+
+  it("can reject a request with valid JWT, other user name", async () => {
+    const response = await request(app)
+      .delete("/friends/reject/user1")
+      .set("jwt", encryptedUser2JWT)
+      .send({});
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toHaveProperty("message");
+    expect(response.body.message).toEqual(
+      "Friend request rejected successfully"
+    );
+
+    encryptedUser1JWT = response.body.jwt;
+
+    requestingUser = await User.findOne({ username: "user1" });
+    rejectingUser = await User.findOne({ username: "user2" });
+    expect(requestingUser).toHaveProperty("requestedFriends");
+    expect(rejectingUser).toHaveProperty("receivedFriends");
+    expect(requestingUser.requestedFriends.length).toEqual(0);
+    expect(rejectingUser.receivedFriends.length).toEqual(0);
+  });
+
+  it("error code on invalid JWT", async () => {
+    const response = await request(app)
+      .delete("/friends/reject/user1")
+      .set("jwt", encryptedUser1JWT + 1)
+      .send({});
+    expect(response.statusCode).toEqual(500);
+    expect(response.body).toHaveProperty("error");
+  });
+
+  it("error code on invalid other username", async () => {
+    const response = await request(app)
+      .delete("/friends/reject/user")
+      .set("jwt", encryptedUser1JWT)
+      .send({});
+    expect(response.statusCode).toEqual(500);
+    expect(response.body).toHaveProperty("error");
+  });
+
+  it("error code when trying to reject a non-existent request", async () => {
+    const response = await request(app)
+      .delete("/friends/reject/user1")
+      .set("jwt", encryptedUser2JWT)
+      .send({});
+    expect(response.statusCode).toEqual(500);
+    expect(response.body).toHaveProperty("error");
+  });
+});
